8552e1ce8440dc289e24b3fb779c93a728c6ba28
Lurumad <luis_ruiz_pavon@hotmail.com>
2016-10-14 10:46:25 +0200

    Change port

--- src/CoreWebApp/Program.cs
+++ src/CoreWebApp/Program.cs
@@ -13,6 +13,7 @@ namespace CoreWebApp
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://*:80")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
